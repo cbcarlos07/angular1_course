7217eeda51ec8e8c391f46e857dc0b1ba15f4ec6
Carlos Bruno <cbcarlos08@gmail.com>
2019-01-21 18:58:30 -0400

    Seção 13

--- app/billingCycle/billingCycleController.js
+++ app/billingCycle/billingCycleController.js
@@ -6,10 +6,18 @@
     ])
     function BillingCycleController($http, msgs){
         const vm = this
+        const url = 'http://localhost:3003/api/billingCycles'
+        vm.refresh = function(){
+           $http.get(url).then(function(response){
+               vm.billingCycle = {}
+               vm.billingCycle = response.data
+           }) 
+        }
+
         vm.create = function(){
-            const url = 'http://localhost:3003/api/billingCycles'
+           
             $http.post(url, vm.billingCycle).then(function(response){
-                vm.billingCyle = {}
+                vm.refresh()
                 msgs.addSuccess('Operação realizada com sucesso!')
                 console.log('Sucesso!')
             }).catch(function(response){
@@ -17,5 +25,6 @@
                 msgs.addError( response.data.errors )                
             })          
         }
+        vm.refresh()
     }
 })()
