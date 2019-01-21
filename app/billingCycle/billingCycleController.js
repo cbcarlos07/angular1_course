b45c7d07d9689f19e2519a18a752365e695dad98
Carlos Bruno <cbcarlos08@gmail.com>
2019-01-21 12:33:48 -0400

    Seção 12

--- app/billingCycle/billingCycleController.js
+++ app/billingCycle/billingCycleController.js
@@ -1,17 +1,21 @@
 (function(){
     angular.module('primeiraApp').controller('BillingCycleCtrl',[  
         '$http',
+        'msgs',
         BillingCycleController
     ])
-    function BillingCycleController($http){
+    function BillingCycleController($http, msgs){
         const vm = this
         vm.create = function(){
             const url = 'http://localhost:3003/api/billingCycles'
             $http.post(url, vm.billingCycle).then(function(response){
                 vm.billingCyle = {}
+                msgs.addSuccess('Operação realizada com sucesso!')
                 console.log('Sucesso!')
-            })
-          
+            }).catch(function(response){
+                console.log('error', response)
+                msgs.addError( response.data.errors )                
+            })          
         }
     }
 })()
